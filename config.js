4a695c39426cd6eb9a2ee8f824c0b772aac0d6ef
Harsh R J <107819939+jogiharsh@users.noreply.github.com>
2026-09-17 14:36:46 +1000

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 const PORTAL_PASSWORD = 'Hrj@1993';
 
 const SUPABASE_URL = 'https://hdedxguhmilzlilybxgc.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkZWR4Z3VobWlsemxpbHlieGdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NjI3MjcsImV4cCI6MjA5NDIzODcyN30.EH7R_wH5pGnhjFEfEb4YcaZcVpKbAe8uPjrx27MIRaw';
+const SUPABASE_ANON_KEY = 'sb_publishable_Fc0aE2unBnK4AQF4xKRHcg_xzGd_7tx';
